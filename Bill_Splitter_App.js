9c60a3a9d3ad22c5790d253ff8428b23e824fff4
Jorge Bernal <jorgebernal@usf.edu>
2024-09-08 21:01:09 -0400

    Add output functionality to display bill, tip, and total

--- Bill_Splitter_App.js
+++ Bill_Splitter_App.js
@@ -1,5 +1,10 @@
-// Task 1 Calculate Tip
+// Task 1: Calculate Tip
 
 let bill = 100;
 let tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
-console.log(`Bill: $${bill}, Tip: $${tip.toFixed(2)}`);
+console.log(`Bill: $${bill}, Tip: $${tip}`);
+
+// Task 2: Output Details
+
+let total = bill + tip;
+console.log(`The bill was $${bill},the tip was $${tip}, and the total value is $${total}`);
