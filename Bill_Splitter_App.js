15bb33ffced98da7c6ac70b362c6b077178b928e
Jorge Bernal <jorgebernal@usf.edu>
2024-09-08 21:10:33 -0400

    Create and test calculateTip function

--- Bill_Splitter_App.js
+++ Bill_Splitter_App.js
@@ -7,4 +7,17 @@ console.log(`Bill: $${bill}, Tip: $${tip}`);
 // Task 2: Output Details
 
 let total = bill + tip;
-console.log(`The bill was $${bill},the tip was $${tip}, and the total value is $${total}`);
+console.log(`The bill was $${bill}, the tip was $${tip}, and the total value is $${total}`);
+
+// Task 3: Create a Function calculateTip
+
+function calculateTip(bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+
+let bill = 100;
+let tip = calculateTip(bill);
+console.log(`The bill was $${bill}, the tip was $${tip}, and the total value is $${bill+tip}`);
+
+
+
